5153a202209ebbe2331baad922bdb4f0708a15db
Vikentsi Lapa <Vikentsi_Lapa@epam.com>
2020-11-08 23:32:35 +0300

    PXE server examples
    PXE boot to CentOS installation from NFS
    PXE boot to CentOS with auto installation from NFS

--- centos_pexe/setup_pxe.sh
+++ centos_pexe/setup_pxe.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 
-echo Install cobbler
+echo Install PXE server
 yum -y install epel-release
 
 yum -y install dhcp-server
@@ -11,7 +11,6 @@ firewall-cmd --add-service=tftp
 # disable selinux or permissive
 setenforce 0
 # 
-systemctl start nfs-server.service
 
 cat >/etc/dhcp/dhcpd.conf <<EOF
 option space pxelinux;
@@ -62,11 +61,15 @@ LABEL linux
   menu label ^Install system
   menu default
   kernel images/CentOS-8.2/vmlinuz
-  append initrd=images/CentOS-8.2/initrd.img ip=dhcp 
+  append initrd=images/CentOS-8.2/initrd.img ip=enp0s3:dhcp inst.repo=nfs:10.0.0.20:/mnt/centos8-install
+LABEL linux-auto
+  menu label ^Auto install system
+  kernel images/CentOS-8.2/vmlinuz
+  append initrd=images/CentOS-8.2/initrd.img ip=enp0s3:dhcp inst.ks=nfs:10.0.0.20:/home/vagrant/cfg/ks.cfg inst.repo=nfs:10.0.0.20:/mnt/centos8-autoinstall
 LABEL vesa
   menu label Install system with ^basic video driver
   kernel images/CentOS-8.2/vmlinuz
-  append initrd=images/CentOS-8.2/initrd.img ip=dhcp inst.xdriver=vesa nomodeset 
+  append initrd=images/CentOS-8.2/initrd.img ip=dhcp inst.xdriver=vesa nomodeset
 LABEL rescue
   menu label ^Rescue installed system
   kernel images/CentOS-8.2/vmlinuz
@@ -80,3 +83,74 @@ mkdir -p /var/lib/tftpboot/pxelinux/images/CentOS-8.2/
 curl -O http://ftp.mgts.by/pub/CentOS/8.2.2004/BaseOS/x86_64/os/images/pxeboot/initrd.img
 curl -O http://ftp.mgts.by/pub/CentOS/8.2.2004/BaseOS/x86_64/os/images/pxeboot/vmlinuz
 cp {vmlinuz,initrd.img} /var/lib/tftpboot/pxelinux/images/CentOS-8.2/
+
+
+# Setup NFS auto install
+# 
+
+curl -O http://ftp.mgts.by/pub/CentOS/8.2.2004/BaseOS/x86_64/os/images/boot.iso
+mkdir /mnt/centos8-install
+mount -t iso9660 boot.iso /mnt/centos8-install
+echo '/mnt/centos8-install *(ro)' > /etc/exports
+systemctl start nfs-server.service
+
+
+autoinstall(){
+  # to speedup replace URL with closest mirror
+  curl -O http://ftp.mgts.by/pub/CentOS/8.2.2004/isos/x86_64/CentOS-8.2.2004-x86_64-minimal.iso
+  mkdir /mnt/centos8-autoinstall
+  mount -t iso9660 CentOS-8.2.2004-x86_64-minimal.iso /mnt/centos8-autoinstall
+  echo '/mnt/centos8-autoinstall *(ro)' >> /etc/exports
+  mkdir /home/vagrant/cfg
+cat > /home/vagrant/cfg/ks.cfg <<EOF
+#version=RHEL8
+ignoredisk --only-use=sda
+autopart --type=lvm
+# Partition clearing information
+clearpart --all --initlabel --drives=sda
+# Use graphical install
+graphical
+# Keyboard layouts
+keyboard --vckeymap=us --xlayouts='us'
+# System language
+lang en_US.UTF-8
+#repo
+#url --url=http://ftp.mgts.by/pub/CentOS/8.2.2004/BaseOS/x86_64/os/
+
+# Network information
+network  --bootproto=dhcp --device=enp0s3 --ipv6=auto --activate
+network  --bootproto=dhcp --device=enp0s8 --onboot=off --ipv6=auto --activate
+network  --hostname=localhost.localdomain
+# Root password
+rootpw --iscrypted $6$g4WYvaAf1mNKnqjY$w2MtZxP/Yj6MYQOhPXS2rJlYT200DcBQC5KGWQ8gG32zASYYLUzoONIYVdRAr4tu/GbtB48.dkif.1f25pqeh.
+# Run the Setup Agent on first boot
+firstboot --enable
+# Do not configure the X Window System
+skipx
+# System services
+services --enabled="chronyd"
+# System timezone
+timezone America/New_York --isUtc
+user --groups=wheel --name=val --password=$6$ihX1bMEoO3TxaCiL$OBDSCuY.EpqPmkFmMPVvI3JZlCVRfC4Nw6oUoPG0RGuq2g5BjQBKNboPjM44.0lJGBc7OdWlL17B3qzgHX2v// --iscrypted --gecos="val"
+
+%packages
+@^minimal-environment
+kexec-tools
+
+%end
+
+%addon com_redhat_kdump --enable --reserve-mb='auto'
+
+%end
+
+%anaconda
+pwpolicy root --minlen=6 --minquality=1 --notstrict --nochanges --notempty
+pwpolicy user --minlen=6 --minquality=1 --notstrict --nochanges --emptyok
+pwpolicy luks --minlen=6 --minquality=1 --notstrict --nochanges --notempty
+%end
+EOF
+echo '/home/vagrant/cfg *(ro)' >> /etc/exports
+  systemctl reload nfs-server.service
+}
+# uncomment to enable automatic installation
+#autoinstall
